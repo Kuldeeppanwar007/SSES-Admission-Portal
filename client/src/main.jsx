19d6be417b473bad2e8defee998140ad1fe307d6
anees6265 <aneeskhan@ssism.org>
2026-04-05 16:31:45 +0530

    app zoom disabled

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,9 @@ import './index.css';
 import App from './App.jsx';
 
 if (Capacitor.isNativePlatform()) {
+  // Zoom disable karo sirf app mein
+  const viewport = document.querySelector('meta[name=viewport]');
+  if (viewport) viewport.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';
   SplashScreen.hide({ fadeOutDuration: 500 });
 }
 
